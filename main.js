d9d98387f8f68acfff448b89a94bc67bec27442d
Mattisbbn <mattisbbn@gmail.com>
2024-04-28 18:43:47 +0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const text = document.getElementById("text").innerHTML;
+document.getElementById("text").innerText = ""; // Efface le texte
 function typing() {
-  //   Code
-}
+let i = 0;   
+ 
+
+ setInterval(() => {
+    if (i < text.length) {
+      document.getElementById("text").innerHTML += text[i];
+      i++;
+    }}, 50); 
+  }
